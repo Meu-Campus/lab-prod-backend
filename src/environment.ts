ae31cff5e79374f699b0e44ba2180ea1627f4943
Vinicius <vavizvini000@gmail.com>
2025-06-29 21:47:44 -0300

    feat: adiciona base do server express

---
+++ src/environment.ts
@@ -0,0 +1,6 @@
+﻿import * as process from "node:process";
+
+export const environment = {
+  port: parseInt(process.env.PORT ?? "4000"),
+  host: process.env.HOST ?? 'localhost',
+}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+﻿import { logger } from "io-logger";
+import express from "express";
+import { environment } from "./environment";
+import cors from "cors";
+
+const main = async () => {
+  logger.info('Starting the application...');
+  const server = express();
+
+  server.use(express.json());
+  server.use(cors());
+
+  server.listen(environment.port, environment.host, () => {
+    logger.info(`Server is running on http://${environment.host}:${environment.port}`);
+  });
+}
+
+main();
